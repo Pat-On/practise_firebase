6a80c472b2b8b3015f72d99dbdb82864a3304b5e
patryk <patryk.r.nowak@gmail.com>
2021-08-09 21:33:06 +0100

    added where filter inside cars -> index.js to follow the rules in firebase

--- src/components/cars/index.js
+++ src/components/cars/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { db, siteRef, employeeRef } from "../../utils/firebase";
+import { db, siteRef, employeeRef, carCollection } from "../../utils/firebase";
 import { firebaseLooper } from "../../utils/tools";
 import Form from "./forms";
 
@@ -7,21 +7,24 @@ const Cars = () => {
   const [cars, setCars] = useState([]);
 
   useEffect(() => {
-    db.collection("cars")
+    console.log("?");
+    // db.collection("cars");
+    carCollection
+      // we need to ad where classes if we put rules with some additional blockaded
+      .where("available", "==", true)
       .orderBy("price", "asc")
-      .startAt(400)
-      .endAt(300000)
+      // .startAt(400)
+      // .endAt(300000)
       // .startAfter(400) //401
       // .endBefore(1000) //999
       // .limit(2) // limiter
       // .limitToLast(2) // limit 2 but from the end
       .get()
       .then((snapshot) => {
+        console.log(snapshot);
         const cars = firebaseLooper(snapshot);
-        // console.log(cars);
-        setCars((prevState) => {
-          return cars;
-        });
+        console.log(cars);
+        setCars(cars);
         // snapshot.forEach((doc) => {
         //   console.log(doc.data());
         //   console.log(doc.id);
@@ -31,71 +34,88 @@ const Cars = () => {
         // console.log(e);
       });
     //get doc by id
-    db.collection("cars")
-      .doc("0SWgXHhXk2rUbR1IwEUO")
-      // .doc()
-      // .limit(2)
-      .get()
-      .then((snapshot) => {
-        // console.log(snapshot.data());
-      });
+    // db.collection("cars")
+    //   //
+    //   .doc("0SWgXHhXk2rUbR1IwEUO")
+    //   // .doc()
+    //   // .limit(2)
+    //   .get()
+    //   .then((snapshot) => {
+    //     console.log(snapshot.data());
+    // });
 
-    siteRef.get().then((querySnapshot) => {
-      // console.log(querySnapshot.data());
-      //   console.log(snapshot); the same
-    });
-    employeeRef.get().then((querySnapshot) => {
-      const employee = firebaseLooper(querySnapshot);
-      // console.log(employee);
-      //   console.log(snapshot); the same
-    });
+    // siteRef.get().then((querySnapshot) => {
+    //   // console.log(querySnapshot.data());
+    //   //   console.log(snapshot); the same
+    // });
+    // employeeRef.get().then((querySnapshot) => {
+    //   const employee = firebaseLooper(querySnapshot);
+    //   // console.log(employee);
+    //   //   console.log(snapshot); the same
+    // });
 
     // to check if something exist or no
-    db.collection("cars")
-      .doc("sss")
-      .get()
-      .then((snapshot) => {
-        // console.log(snapshot.exists); // byt this you can get info if it exist or no => nice
-      })
-      .catch((e) => {
-        // console.log(e);
-      });
+    // db.collection("cars")
+    //   .doc("sss")
+    //   .get()
+    //   .then((snapshot) => {
+    //     // console.log(snapshot.exists); // byt this you can get info if it exist or no => nice
+    //   })
+    //   .catch((e) => {
+    //     // console.log(e);
+    //   });
   }, []);
 
   const getAllCars = () => {
-    db.collection("cars")
-      // where // operator // and equal to what
-      .where("color", "!=", "red")
+    console.log("?");
+    // db.collection("cars")
+    carCollection
+      .where("available", "==", true)
+      .orderBy("price", "asc")
       .get()
       .then((snapshot) => {
-        // console.log(snapshot.data()); //wrong
+        console.log(snapshot);
         const cars = firebaseLooper(snapshot);
-        cars.forEach((item) => console.log(item)); // printing each item
-        // console.log(firebaseLooper(snapshot)); // printing array
+        console.log(cars);
+        setCars(cars);
+      })
+      .catch((e) => {
+        console.log(e);
       });
 
-    db.collection("cars")
-      // where // operator // and equal to what
-      .where("price", ">=", 1000)
-      .where("price", "<=", 5000)
-      .get()
-      .then((snapshot) => {
-        // console.log(snapshot.data()); //wrong
-        const cars = firebaseLooper(snapshot);
-        cars.forEach((item) => console.log(item)); // printing each item
-        // console.log(firebaseLooper(snapshot)); // printing array
-      });
+    // db.collection("cars")
+    //   // where // operator // and equal to what
+    //   .where("color", "!=", "red")
+    //   .get()
+    //   .then((snapshot) => {
+    //     // console.log(snapshot.data()); //wrong
+    //     const cars = firebaseLooper(snapshot);
+    //     cars.forEach((item) => console.log(item)); // printing each item
+    //     // console.log(firebaseLooper(snapshot)); // printing array
+    //   });
 
-    db.collection("cars")
-      // by what and asc or desc
-      .orderBy("price", "desc") //asc
-      .get()
-      .then((snapshot) => {
-        // console.log(snapshot.data()); //wrong
-        const cars = firebaseLooper(snapshot);
-        cars.forEach((item) => console.log(item)); // printing each item
-        // console.log(firebaseLooper(snapshot)); // printing array
-      });
+    // db.collection("cars")
+    //   // where // operator // and equal to what
+    //   .where("price", ">=", 1000)
+    //   .where("price", "<=", 5000)
+    //   .get()
+    //   .then((snapshot) => {
+    //     // console.log(snapshot.data()); //wrong
+    //     const cars = firebaseLooper(snapshot);
+    //     cars.forEach((item) => console.log(item)); // printing each item
+    //     // console.log(firebaseLooper(snapshot)); // printing array
+    //   });
+
+    // db.collection("cars")
+    //   // by what and asc or desc
+    //   .orderBy("price", "desc") //asc
+    //   .get()
+    //   .then((snapshot) => {
+    //     // console.log(snapshot.data()); //wrong
+    //     const cars = firebaseLooper(snapshot);
+    //     cars.forEach((item) => console.log(item)); // printing each item
+    //     // console.log(firebaseLooper(snapshot)); // printing array
+    //   });
   };
 
   const handleCarData = (cars) => {
